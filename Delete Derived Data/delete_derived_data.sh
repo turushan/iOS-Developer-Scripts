bb012f994e6bf7e1d0d5a33a2fb94f858bbd8069
Turushan Aktay <turushanaktay@gmail.com>
2019-01-18 13:07:11 +0300

    Refactor scripts' loggings.

--- Delete Derived Data/delete_derived_data.sh
+++ Delete Derived Data/delete_derived_data.sh
@@ -3,5 +3,7 @@
 echo "Script is initialized"
 echo "Closing Xcode..."
 killall "Xcode"
+echo "Xcode is closed"
+echo "Running cleaning..."
 rm -rf ~/Library/Developer/Xcode/DerivedData
 echo "Derived data is deleted, for more iOS Developer scripts visit https://github.com/turushan/iOS-Developer-Scripts"
--- Delete Old Unsupported Simulators/delete_old_unsupported_simulators.sh
+++ Delete Old Unsupported Simulators/delete_old_unsupported_simulators.sh
@@ -3,5 +3,7 @@
 echo "Script is initialized"
 echo "Closing Xcode..."
 killall "Xcode"
+echo "Xcode is closed"
+echo "Running cleaning..."
 xcrun simctl delete unavailable
 echo "Old unsupported simulators are deleted, for more iOS Developer scripts visit https://github.com/turushan/iOS-Developer-Scripts"
